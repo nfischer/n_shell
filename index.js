60584905fdd710417aa1444769fc5f70576af6b0
Jessie France <FranceJessieJ@gmail.com>
2022-02-20 12:51:18 -0700

    chore: apply new linting

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-'use strict';
 
 // ShellJS plugins
 require('shelljs-plugin-clear');
@@ -7,20 +6,19 @@ require('shelljs-plugin-inspect');
 require('shelljs-plugin-open');
 require('shelljs-plugin-sleep');
 
-var util = require('util');
-var repl = require('repl');
-var argv = require('minimist')(process.argv.slice(2));
-var replHistory = require('repl.history');
-var osHomedir = require('os-homedir');
-var path = require('path');
+const util = require('util');
+const repl = require('repl');
+const argv = require('minimist')(process.argv.slice(2));
+const replHistory = require('repl.history');
+const osHomedir = require('os-homedir');
+const path = require('path');
 
-var shell;
-var json;
-var isLocal;
+let shell;
+let json;
+let isLocal;
 try {
-  if (argv.path[0] === '~')
-    argv.path = argv.path.replace('~', osHomedir());
-  var localShellJS = path.resolve(argv.path);
+  if (argv.path[0] === '~') argv.path = argv.path.replace('~', osHomedir());
+  const localShellJS = path.resolve(argv.path);
   shell = require('require-relative')(localShellJS, process.cwd());
   json = require(path.join(localShellJS, 'package.json'));
   isLocal = true;
@@ -31,82 +29,79 @@ try {
 }
 
 // Create the prompt
-var myprompt = argv.prompt || 'shelljs %v%l $ ';
-myprompt = myprompt.replace(/%./g, (function() {
-  var option = {
+let myprompt = argv.prompt || 'shelljs %v%l $ ';
+myprompt = myprompt.replace(/%./g, (function fn() {
+  const option = {
     '%%': '%',
     '%v': json.version,
     '%l': (isLocal ? ' [local]' : '')
   };
-  return function(match) {
-    return option[match];
-  };
-})());
+  return (match) => option[match];
+}()));
 
-var replServer = repl.start({
+const replServer = repl.start({
   prompt: myprompt,
   replMode: process.env.NODE_REPL_MODE === 'strict' || argv.use_strict ? repl.REPL_MODE_STRICT : repl.REPL_MODE_MAGIC
 });
 
 // save repl history
-var HISTORY_FILE = path.join(osHomedir(), '.n_shell_history');
+const HISTORY_FILE = path.join(osHomedir(), '.n_shell_history');
 replHistory(replServer, HISTORY_FILE);
 
 // Newer versions of node use a symbol called util.inspect.custom.
-var inspectAttribute = util.inspect.custom || 'inspect';
+const inspectAttribute = util.inspect.custom || 'inspect';
 
 function wrap(fun, key) {
   if (typeof fun !== 'function') {
     return fun; // not a function
-  } else {
-    var outerRet = function() {
-      var ret = fun.apply(this, arguments);
+  }
+    const outerRet = (...args) => {
+      const ret = fun.apply(this, args);
+      // Polyfill .inspect() method
+      function emptyInspect() {
+        return '';
+      }
+
       if (ret instanceof Object) {
-        // Polyfill .inspect() method
-        function emptyInspect() {
-          return '';
-        }
-        var oldInspect;
+        let oldInspect;
         if (ret[inspectAttribute]) {
           oldInspect = ret[inspectAttribute].bind(ret);
         } else {
-          oldInspect = function() { return ''; }
+          oldInspect = () => '';
         }
         if (key === 'echo' || key === 'exec') {
           ret[inspectAttribute] = emptyInspect;
         } else if (key === 'pwd' || key === 'which') {
-          ret[inspectAttribute] = function () {
-            var oldResult = oldInspect();
+          ret[inspectAttribute] = () => {
+            const oldResult = oldInspect();
             return oldResult.match(/\n$/) ? oldResult : oldResult + '\n';
           };
         }
       }
       return ret;
     };
-    outerRet[inspectAttribute] = outerRet[inspectAttribute] || function () { return this(); };
+  outerRet[inspectAttribute] = outerRet[inspectAttribute] || function thisFn() { return this(); };
     return outerRet;
-  }
 }
 
 argv.no_global = argv.no_global || argv.local || argv.n;
 
 // Add inspect() method, if it doesn't exist
 if (!argv.noinspect) {
-  for (var key in shell) {
+  Object.keys(shell).forEach((key) => {
     try {
       shell[key] = wrap(shell[key], key);
-    } catch (e) {}
-  }
+    } catch (e) { /* empty */ }
+  });
 }
 
 if (argv.no_global) {
-  if (typeof argv.no_global !== 'string')
-    argv.no_global = 'shell';
+  if (typeof argv.no_global !== 'string') argv.no_global = 'shell';
   replServer.context[argv.no_global] = shell;
 } else {
-  for (var key in shell) {
+  Object.keys(shell).forEach((key) => {
     replServer.context[key] = shell[key];
-  }
+  });
 }
 
 module.exports = replServer;
